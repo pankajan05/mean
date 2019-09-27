b218c40dcbcda028fd184113386be3b8807778d2
Pankajan Satkunam <pankajan05@gmail.com>
2019-09-27 04:13:29 -0700

    get the data from the mongodb

--- contact/server.js
+++ contact/server.js
@@ -1,28 +1,17 @@
 var express = require('express');
 var app = express();
+var mongojs = require('mongojs');
+var db = mongojs('contactlist',['contactlist']);
 
 app.use(express.static(__dirname + "/public"));
 
 app.get('/contactlist', function(req, res){
     console.log("I received a Get Request");
 
-    person1 = {
-        fullname: 'pankajan satkunam',
-        student_id: 'SE/2016/032',
-        phone_number: '0775498903',
-        address: 'Earlalai East Earlalai',
-        email_address: 'pankajan05@gmail.com'
-    };
-    person2 = {
-        fullname: 'janaka edirisinghe',
-        student_id: 'SE/2016/007',
-        phone_number: '0775453443',
-        address: 'Polonaruwa',
-        email_address: 'janaka@gmail.com'
-    };
-
-    var contactlist = { person1, person2};
-    res.json(contactlist);
+   db.contactlist.find(function (err, docs) {
+       console.log(docs);
+       res.json(docs);
+   });
 });
 
 app.listen(3000);
