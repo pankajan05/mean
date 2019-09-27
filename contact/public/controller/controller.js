fdb5b65ffda86d4a294dbf4b5a512b531918d4f6
Pankajan Satkunam <pankajan05@gmail.com>
2019-09-27 05:55:02 -0700

    finish the edit details

--- contact/public/controller/controller.js
+++ contact/public/controller/controller.js
@@ -20,4 +20,18 @@ function Appctrl($scope, $http) {
             refresh();
         });
     };
+
+    $scope.edit = function(id){
+        console.log(id);
+        $http.get('/contactlist/' + id).success(function(response){
+            $scope.contact = response;
+        });
+    };
+
+    $scope.update = function() {
+        console.log($scope.contact._id);
+        $http.put('/contactlist/' + $scope.contact._id, $scope.contact).success(function (response) {
+            refresh();
+        });
+    };
 }
--- contact/server.js
+++ contact/server.js
@@ -30,5 +30,21 @@ app.delete('/contactlist/:id', function (req, res) {
     });
 });
 
+app.get('/contactlist/:id',function (req,res) {
+    var id = req.params.id;
+    db.contactlist.findOne({_id: mongojs.ObjectId(id)}, function(err, doc){
+        res.json(doc);
+    });
+});
+
+app.put('/contactlist/:id', function (req, res) {
+    var id = req.params.id;
+    db.contactlist.findAndModify({query: {_id: mongojs.ObjectId(id)},
+        update: {$set: { fullname: req.body.fullname, student_id: req.body.student_id, phone_number: req.body.phone_number, address: req.body.address, email_address: req.body.email_address }},
+        new: true}, function (err, doc) {
+            res.json(doc);
+    });
+});
+
 app.listen(3000);
 console.log("Server running on port 3000");
